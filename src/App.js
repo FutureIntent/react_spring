785718cec0da1cfcdeff233697c74ad06b2c66f7
FutureIntent <IamSoulfuller@gmail.com>
2022-06-26 20:38:24 +0300

    Fresh upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+    const [res, setRes] = useState();
+
+    function testAuth() {
+        fetch('http://localhost:8080/user/cors',
+            {
+                method: 'POST',
+                mode: 'cors',
+                redirect: 'follow',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    word: 'zzz'
+                })
+            }
+        )
+            .then(async response => {               
+                if (response.redirected) {
+                    window.location.href = response.url;
+                }
+                const res = await response.json();
+                console.log(res);
+            })           
+            .catch(err => console.log(err))
+    }
+
+    function logOut() {
+        window.location.href = "http://localhost:8080/logout";
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+          <p>Placeholder</p>
+          <button onClick={testAuth}>Test Auth</button>
+          <button onClick={logOut}>Log Out</button>
     </div>
   );
 }
